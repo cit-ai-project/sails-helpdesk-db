7326e5005fa3ee89bd8bfbc6a407779a788c81aa
Clement <Clement.R@congruentindia.com>
2018-01-05 14:47:43 +0530

    Second commit

---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    user_id: {
+      type: 'string',
+      unique: true,
+      primaryKey: true
+     },
+    user_name: 'string',
+    user_email: 'string',
+    first_name: 'string',
+    last_name: 'string',
+    DOB: 'date'
+
+  }
+};
+
